0c89c67e33f4b53b4ecaf73e667cf78a47fda98d
Katherine_20 <mob1911494@gmail.com>
2022-05-25 21:18:25 +0300

    задает массив из 8 элементов

---
+++ No29/Program.cs
@@ -0,0 +1,15 @@
+﻿// задает массив из 8 элементов и выводит его на экран
+int [] ArrayCreation (int length)
+{
+    int[] arr = new int[length];
+    Random rnd = new Random();
+
+    for(int i=0; i < length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+int[] array = ArrayCreation(8);
+Console.Write(string.Join(",", array));

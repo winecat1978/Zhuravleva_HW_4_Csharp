957ac3dea9c3a83028547ae817af376ab5d86c40
Katherine_20 <mob1911494@gmail.com>
2022-05-25 21:19:17 +0300

    working with text elements replacement

---
+++ example2/Program.cs
@@ -0,0 +1,32 @@
+﻿// заменить все пробелы черточками, все маленькие к на К, С на с.
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+            // string s = "qwerty"
+            //             012
+            // s[3] r 
+
+            string Replace (string text, char oldValue, char newValue)
+            {
+                string result = String.Empty; // иницилизация пустой строки
+
+                int length = text.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if(text[i] == oldValue) 
+                    {
+                        result = result + $"{newValue}";
+                    }
+                    else result = result + $"{text[i]}";
+                }
+                return result;
+            }
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+string newestText = Replace(newText, 'к', 'K');
+Console.WriteLine(newestText);
